757d90982b770653f1f6863b85ee06560fa2755e
David Dembinski <darkvplayer@gmail.com>
2023-02-01 22:25:40 +0100

    feat: fix json output issues feat: add function for interrogating images in a folder feat: add function for filtering filenames to images only feat: add simple app image setup: updated README.md on usage

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from clip_interrogator import Interrogator, Config
 import os
 import time
 import typer
-from json import dumps
+import json
 
 
 APP = typer.Typer()
@@ -50,7 +50,7 @@ def interrogate_image(file_index: int, filepath: str, CI: Interrogator, model_ty
             print("Image", file_index, "Output", output_path)
             image.save(output_path, quality=100, subsampling=0)
         else:
-            return dumps({"index": file_index, "caption": stripped_caption, "filetype": ".png"})
+            return json.dumps({"index": file_index, "caption": stripped_caption, "filetype": ".png"}, ensure_ascii=False)
 
 
 def create_session_dirs(output_path, session_path):
@@ -65,6 +65,11 @@ def create_session_dirs(output_path, session_path):
 
 def test_callback(input):
     return input if input in ["fast", "best"] else "fast"
+
+
+def filter_files(raw_filenames: list[str]):
+    valid_extensions = [".png", ".jpg", ".jpeg", ".gif", ".bmp"]
+    return list(filter(lambda file: any([extension in file.lower() for extension in valid_extensions]), raw_filenames))
 
 
 @APP.command()
@@ -84,16 +89,24 @@ def interrogate(files: List[str], model_type: ModelType = ModelType.fast, output
 
         print("Warming up stable-cheese")
         print("On first run this step it can take a while because the models have to process the tokens for the interrogating once")
-        CI = Interrogator(Config(
-            clip_model_name="ViT-H-14/laion2b_s32b_b79k", blip_max_length=15, device="cuda"))
+        CI = Interrogator(Config(clip_model_name="ViT-H-14/laion2b_s32b_b79k", blip_max_length=15, device="cuda"))
         json_output = []
         print("Processing started")
         for filepath in files:
             index += 1
             json_output.append(interrogate_image(index, filepath, CI, model_type, session_path))
         print("Processing finshed")
-        print(dumps(json_output))
+        print(json_output)
         print("")
+        
+
+@APP.command()
+def interrogate_folder(folder_path: str, model_type: ModelType = ModelType.fast, output_files: bool = typer.Option(False)):
+    if folder_path and len(folder_path) > 0:
+        raw_filenames = os.listdir(folder_path)
+        image_filenames = filter_files(raw_filenames)
+        image_paths = [os.path.join(folder_path, image_filename) for image_filename in image_filenames]
+        interrogate(image_paths, model_type, output_files)
 
 
 if __name__ == '__main__':
